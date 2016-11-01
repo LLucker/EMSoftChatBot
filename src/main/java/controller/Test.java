d5b6471579ef06d5bc7c28b31ea772f622f8704b
Luan Sena <luansena@live.com>
2016-11-01 20:10:37 -0200

    Update Test.java

--- src/main/java/controller/Test.java
+++ src/main/java/controller/Test.java
@@ -7,11 +7,11 @@ import java.util.Scanner;
  * @author luan
  */
 public class Test {
-    public static void main(String[] args) {
+  /*  public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
         //System.out.println("Digite uma duvida:");
         String duvida = "oi";//ler.nextLine();
         String resposta = Brain.consultarPergunta(duvida);
         System.out.println(resposta);
-    }
+    }*/
 }
